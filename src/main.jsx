52deaf1e14dfc17d74eb62c757f860b5813ccd0c
Luis Romero <dev@luisromero.co>
2025-05-18 16:22:13 -0500

    delete css reset

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { scan } from "react-scan";
-import "./reset.css";
 import "./index.css";
 import App from "./App.jsx";
 
